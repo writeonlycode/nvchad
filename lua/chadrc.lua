e0dbba670d4149aa60e88c879ca1cafbce093d0f
Iago Bozza <iagobozza@writeonlycode.io>
2025-02-06 11:06:27 -0300

    Update term and theme configuration

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,19 +6,26 @@
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	theme = "doomchad",
 }
 
--- M.nvdash = { load_on_startup = true }
 M.ui = {
     tabufline = {
       enabled = false,
-    }
+    },
+}
+
+M.term = {
+      winopts = { number = false, relativenumber = false },
+      sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
+      float = {
+        relative = "editor",
+        row = 0.3,
+        col = 0.25,
+        width = 0.5,
+        height = 0.4,
+        border = "none",
+      },
 }
 
 return M
