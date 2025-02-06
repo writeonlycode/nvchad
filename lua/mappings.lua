8a0f1af95ca954df11453c3af1137b00be822c31
Iago Bozza <iagobozza@writeonlycode.io>
2025-02-06 11:21:43 -0300

    Update mappings for terminal

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,19 @@ map("i", "jj", "<ESC>")
 -- nvimtree
 map("n", "<leader>n", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
 map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
+
+-- new terminals
+map("n", "<leader>h", "", { desc = "terminal new horizontal term" })
+map("n", "<leader>v", "", { desc = "terminal new vertical term" })
+
+-- toggleable
+map({ "n", "t" }, "<A-v>", "", { desc = "terminal toggleable vertical term" })
+map({ "n", "t" }, "<A-h>", "", { desc = "terminal toggleable horizontal term" })
+
+map({ "n", "t" }, "<A-i>", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "terminal toggle floating term" })
+
+map({ "n" }, "<leader>t", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "terminal toggle floating term" })
