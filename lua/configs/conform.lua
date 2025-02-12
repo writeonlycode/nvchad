41626a34cb0ce56e444ec5c831ba7fe202cf3d87
Iago Bozza <iagobozza@writeonlycode.io>
2025-02-12 10:43:53 -0300

    Add snippets and other updates to conform

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -3,11 +3,12 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
+    typescriptreact = { "prettier" },
   },
 
   format_on_save = {
     -- These options will be passed to conform.format()
-    timeout_ms = 500,
+    timeout_ms = 4000,
     lsp_fallback = true,
   },
 }
--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,7 @@
 require "nvchad.options"
 
-local opt = vim.opt
+-- Hide command line at the bottom
+vim.opt.cmdheight = 0
 
-opt.cmdheight = 0
+-- LuaSnip with vscode extensions. i.e ~/.vscode/extensions/deinsoftware.vitest-snippets-1.8.0
+vim.g.vscode_snippets_path = vim.fn.stdpath "config" .. "/lua/snippets"
